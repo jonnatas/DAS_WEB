d863717b08a80e3718182a55761215d8473ee9e3
jonnatas <jonatas_lenon@hotmail.com>
2016-11-23 10:10:51 -0200

    create templates

--- reconhece/views.py
+++ reconhece/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def post_list(request):
-	return render(request, 'reconhece/post_list.html', {})
+	posts = Post.objects.all()
+	return render(request, 'reconhece/post_list.html', {'posts':posts})
 
